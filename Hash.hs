38ba03a7ecbc793ae9d6205308dbbe9a0b21a8d3
wilemanstephen <mateiwileman@gmail.com>
2025-07-10 20:43:08 +0200

    hash and updates to prelude
    Starting working on Hash.hs, did like 1/3 of it today, proud of myself
    
    Also updates RedefineFunctionsFromPrelude.hs, which might go through a name change soon xD, but yeah, added more functions to help me out with my other files :D

--- Hash.hs
+++ Hash.hs
@@ -1,1 +1,47 @@
--- Probs gonna do it next, cause why not
+module Hash where
+
+    import RedefinedFunctionsFromPrelude (splitInteger, fromDigits, middle2Digits, fractionalPart)
+    import Data.Char (ord)
+
+    moduloHashing :: Int -> Int -> String
+    moduloHashing k m
+        | m == 0 = error "Can't do modulo by 0"
+        | m < 0 = "h(k) = 0"
+        | otherwise =
+            let h = k `mod` m
+            in "h(k) = " ++ show h
+
+    foldingMethod :: Int -> Int -> String
+    foldingMethod k m
+        | m == 0 = error "Can't do modulo by 0"
+        | m < 0 = "h(k) = 0"
+        | otherwise =
+            let h = split `mod` m
+                split = sum $ splitInteger k 2
+            in "h(k) = " ++ show h
+
+    midSquareMethod :: Int -> Int -> String
+    midSquareMethod k m
+        | m == 0 = error "Can't do modulo by 0"
+        | m < 0 = "h(k) = 0"
+        | otherwise =
+            let h = middle `mod` m
+                kSquared = k^2
+                middle = middle2Digits kSquared
+            in "h(k) = " ++ show h
+    
+    multiplicativeHashing :: Double -> Double -> String
+    multiplicativeHashing k m
+        | m < 0 = "h(k) = 0"
+        | otherwise =
+            let h = floor (frac * m)
+                a = 0.6180339887
+                frac = fractionalPart (k * a)
+            in "h(k) = " ++ show h
+    
+    djb2Hash :: Int -> String -> String
+    djb2Hash k s =
+        let h = foldl (\acc c -> acc * 33 + ord c) k s
+        in "h(k) = " ++ show h
+
+    -- continuation of hash possibly another time xD, next time Polynomial Rolling Hash, Zobrist Hashing, MD5 Hashing and others
--- RedefinedFunctionsFromPrelude.hs
+++ RedefinedFunctionsFromPrelude.hs
@@ -1,5 +1,8 @@
 module RedefinedFunctionsFromPrelude where
 
+    import Data.Char (digitToInt)
+    import Data.List (genericLength)
+
     redefineLength :: [Double] -> Double
     redefineLength [] = 0
     redefineLength (_:xs) = 1 + redefineLength xs
@@ -11,3 +14,28 @@ module RedefinedFunctionsFromPrelude where
 
     isqrt :: Integer -> Integer 
     isqrt = floor.sqrt.fromIntegral
+
+    splitInteger :: Int -> Int -> [Int]
+    splitInteger n s =
+        let digits = map digitToInt (show n)
+            grouped = chunk s digits
+        in map fromDigits grouped
+            where
+                chunk :: Int -> [a] -> [[a]]
+                chunk _ [] = []
+                chunk k xs = take k xs : chunk k (drop k xs)
+    
+    fromDigits :: [Int] -> Int
+    fromDigits = foldl (\acc d -> acc * 10 + d) 0
+
+    middle2Digits :: Int -> Int
+    middle2Digits n
+        | len < 2 = n
+        | even len = fromDigits $ take 2 $ drop (len `div` 2 - 1) digits
+        | otherwise = fromDigits $ take 2 $ drop (len `div` 2 - 1) digits
+            where
+                digits = map digitToInt $ show (abs n)
+                len = length digits
+    
+    fractionalPart :: RealFrac a => a -> a
+    fractionalPart x = snd (properFraction x)
